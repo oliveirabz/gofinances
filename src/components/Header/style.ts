239087e883d47085ad66831d85e6d6e510585bdb
olivpuddin <bububeatriz@icloud.com>
2022-04-30 00:43:16 -0300

    feat: indentation

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,31 +1,31 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.header`
-    background: var(--primary);
+  background: var(--primary);
 `;
 
 export const Content = styled.div`
-    max-width: 1120px;
-    margin: 0 auto; // always keep centered 
+  max-width: 1120px;
+  margin: 0 auto; // always keep centered
 
-    padding: 2rem 1rem 12rem;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  padding: 2rem 1rem 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    button {
-        font-size: 1rem;
-        color: #000000;
-        background: var(--secondary);
-        border: 0;
-        padding: 0 2rem;
-        border-radius: 0.25rem;
-        height: 3rem;
+  button {
+    font-size: 1rem;
+    color: #000000;
+    background: var(--secondary);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
 
-        transition: filter 0.2s;
+    transition: filter 0.2s;
 
-        &:hover {
-            filter: brightness(0.9);
-        }
+    &:hover {
+      filter: brightness(0.9);
     }
+  }
 `;
